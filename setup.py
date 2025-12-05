a06116c6fb8be3039f24e6ddc0b40bcee62b80ae
JoshuaHong0215 <sebin5736@gmail.com>
2025-12-05 13:13:25 +0900

    폴더 구조 및 폴더 설명 작성

--- setup.py
+++ setup.py
@@ -1,2 +1,17 @@
-import os 
-print("hi")
+'''
+structure = {
+    "app": [
+        "routers",      #API 엔드포인트(길안내)
+        "schemas",      #데이터 검증(주문서 양식)
+        "models",       #DB모델(데이터장부)
+        "crud",         #DB동작
+        "service",      #비즈니스 로직
+        "websocket",    #실시간 통신을 담당
+        "core/ros",     #로봇(ROS2) 연결
+        "core/message"  #메세지 형식
+    ]
+}
+'''
+
+
+
