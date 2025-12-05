63a6e44562de7776ecdcf48a8c6a4766dc040430
JoshuaHong0215 <sebin5736@gmail.com>
2025-12-05 17:29:23 +0900

    localhost 8081로 변경함

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 # 서버 객체 생성
 app = FastAPI()
 
-# 기본주소로 접속했을때 실행될 기능 (http://localhost:8080)
+# 기본주소로 접속했을때 실행될 기능 (http://localhost:8081)
 @app.get("/")
 def read_root():
     return "ROBOT CONTROL SYSTEM"
