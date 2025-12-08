300ef6df469fc39dee9b52ba810cd918ac404246
JoshuaHong0215 <sebin5736@gmail.com>
2025-12-08 11:05:24 +0900

    실행 명령어 주석 추가

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 app = FastAPI()
 
 # 기본주소로 접속했을때 실행될 기능 (http://localhost:8081)
+# uvicorn app.main:app --reload --port 8001로 실행
 @app.get("/")
 def read_root():
     return "ROBOT CONTROL SYSTEM"
